37ee59783d1f4ad6cf3897b0cec48965646ce6e5
wannamakeudance <jingxiangzheng@baidu.com>
2022-02-18 16:23:26 +0800

    init article

--- src/react-redux-simple/Connect.js
+++ src/react-redux-simple/Connect.js
@@ -4,14 +4,15 @@ import Context from './Context';
 const connect = (mapStateToProps, mapDispatchToProps) => {
   return (Component) => {
     return class WrapperComponent extends React.Component {
+      
+      // important variable
       static contextType = Context;
-      render() {
-        console.log(this.context);
 
+      render() {
         return (
           <Component
             {...this.props}
-            // Don't forget to use getState
+            // Don't forget to use getState!
             {...mapStateToProps(this.context.getState())}
             {...mapDispatchToProps(this.context.dispatch)}
           ></Component>
